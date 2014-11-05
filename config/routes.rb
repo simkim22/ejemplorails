49a03895a078efd5da9f465cd30d04cf843eed5c
simkim22 <simkim.22@gmail.com>
2014-11-05 16:32:57 +0100

    finish chapter 10

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
+  get 'account_activation/edit'
+
   # resources :users
   # resources :sessions, only: [:new, :create, :destroy]
   # root  'static_pages#home'
@@ -18,6 +24,8 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,18 @@ User.create!(name:  "Example User",
              email: "juanace@hotmai3l.com",
              password:              "barcelona",
              password_confirmation: "barcelona",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
